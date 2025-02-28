b55349a9ceaca9bc674bc6afe2f0c5c7600914ff
James Dudley <dudley@iu.edu>
2025-02-27 22:30:38 -0500

    initial refactoring

--- ctsi/functions.php
+++ ctsi/functions.php
@@ -132,98 +132,194 @@ function themename_custom_logo_setup() {
 }
 add_action('after_setup_theme', 'themename_custom_logo_setup');
 
-function create_bootstrap_menu($theme_location)
-{
-    $menu_list = '';
-    $column_one = array();
-    $column_two = array();
-    $menu_id = get_nav_menu_locations()[$theme_location];
-    $mega_menus = wpse_nav_menu_2_tree($menu_id);
-    // var_dump($mega_menus);
+function create_bootstrap_menu($theme_location) {
+
+	$menu_list = '';
+ 	$column_one = array();
+ 	$column_two = array();
+  $menu_id = get_nav_menu_locations()[$theme_location];
+  $mega_menus = wpse_nav_menu_2_tree($menu_id);
+  // var_dump($mega_menus);
 
 
-    $menu_list .= '<ul class="navbar-nav">' . "\n";
+	$menu_list .= '<ul class="navbar-nav">' . PHP_EOL;
 
-    foreach ($mega_menus as $key => $value) {
-        $column_choice = get_field('column_choice', $value);
-        $column_one = array();
-        $column_two = array();
-        $target =  $value->target === '_blank' ? '_blank' : '_self';
-        $left_content_text_area = get_field('left_content_text_area', $value);
+	foreach($mega_menus as $key => $value) {
+    $column_choice  = get_field('column_choice', $value);
+    $column_one     = array();
+    $column_two     = array();
+    $target         =  $value->target === '_blank' ? '_blank' : '_self';
+    $left_content_text_area = get_field('left_content_text_area', $value);
 
-        if (count($value->wpse_children) == 0) {
-            $menu_list .= '<li class="nav-item"><a class="nav-link" href="javascript:void(0);">' . $value->title . '</a></li>' . "\n";
-        } else {
-            $menu_list .= '<li class="nav-item dropdown has-megamenu"> <a class="nav-link dropdown-toggle" href="javascript:void(0);" data-bs-toggle="dropdown">' . $value->title . '</a>' . "\n";
-            $menu_list .= '<div class="dropdown-menu megamenu" role="menu">' . "\n";
-            $menu_list .= '<div class="container">' . "\n";
-            $menu_list .= '<div class="closeNavButton_Wrapper hidden_Mobile_header">' . "\n";
-            $menu_list .= '<a class="closeNavButton" data-bs-toggle="dropdown"><img src="/wp-content/themes/ctsi/assets/images/close.svg" alt="close"></a>' . "\n";
-            $menu_list .= '</div>' . "\n";
-            $menu_list .= '<div class="row g-0">' . "\n";
+    if(count($value->wpse_children) == 0) {
+      $menu_list .=
+          '<li class="nav-item">'
+        .   '<a class="nav-link" href="javascript:void(0);">'
+        .     $value->title
+        .   '</a>'
+				.   '<div class="indicator"></div> '.PHP_EOL
+        . '</li>'
+        . PHP_EOL;
+    }
+    else {
+      $menu_list .=
+          '<li class="nav-item dropdown has-megamenu">'
+        .   '<a class="nav-link dropdown-toggle" '
+        .   '   href="javascript:void(0);" '
+        .   '   data-bs-toggle="dropdown">'
+        .     $value->title
+				.   '<div class="indicator"></div> '.PHP_EOL
+        .   '</a>'
+        .   PHP_EOL
+        .   '<div class="dropdown-menu megamenu" role="menu">'
+        .   PHP_EOL
+        .     '<div class="container">'
+        .     PHP_EOL
+        .       '<div class="closeNavButton_Wrapper hidden_Mobile_header">'
+        .       PHP_EOL
+        .         '<a class="closeNavButton" data-bs-toggle="dropdown">'
+        .           '<img src="/wp-content/themes/ctsi/assets/images/close.svg"'
+        .                 'alt="close">'
+        .         '</a>'
+        .         PHP_EOL
+        .       '</div>'
+        .       PHP_EOL
+        .       '<div class="row g-0">'
+        .       PHP_EOL
+        ;
+      if($column_choice == "left" && !empty($left_content_text_area)) {
+        $menu_list .=
+              '<div class="col-lg-4 col-12 megamenu_Details hidden_Mobile_header">'
+          .   PHP_EOL
+          .     '<div class="col-megamenu">'
+          .     PHP_EOL
+          .       '<a   role="menuitem" '
+          .             'target="'.$target.'" '
+          .             'href="' . $value->url . '" '
+          .             'class="megamenu_Details_Title">'
+          .           $value->title
+          .           '<svg xmlns="http://www.w3.org/2000/svg" '
+          .                 'width="16" '
+          .                 'height="16" '
+          .                 'fill="#990000" '
+          .                 'class="bi bi-chevron-right" '
+          .                 'viewBox="0 0 16 16"> '
+          .             '<path fill-rule="evenodd" '
+          .                   'd="M4.646 1.646a.5.5 0 0 '
+          .                   '1 .708 0l6 6a.5.5 0 0 1 0 '
+          .                   '.708l-6 6a.5.5 0 0 '
+          .                   '1-.708-.708L10.293 8 4.646 '
+          .                   '2.354a.5.5 0 0 1 0-.708" /> '
+          .           '</svg>'
+          .         '</a>'
+          .       $left_content_text_area
+          .     PHP_EOL
+          .     '</div>'
+          .     PHP_EOL
+          .   '</div>'
+          .   PHP_EOL
+          ;
+      }
 
-                if ($column_choice == "left" && !empty($left_content_text_area)) {
-                    $menu_list .= '<div class="col-lg-4 col-12 megamenu_Details hidden_Mobile_header">' . "\n";
-                    $menu_list .= '<div class="col-megamenu">' . "\n";
-                    $menu_list .= '<a role="menuitem" target="'.$target.'" href="' . $value->url . '" class="megamenu_Details_Title">' . $value->title . '<svg xmlns="http://www.w3.org/2000/svg"
-                            width="16" height="16" fill="#990000" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd"
-                            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
-                            </svg></a>' . $left_content_text_area . "\n";
-                    $menu_list .= '</div>' . "\n";
-                    $menu_list .= '</div>' . "\n";
-                }
+      foreach($value->wpse_children as $key => $children) {
+        $column_choice          = get_field('column_choice', $children);
+        $menu_link_description  = get_field(
+          'menu_link_description',
+          $children
+        );
+        $description            =     !empty($menu_link_description)
+                                  ?   '<br>'
+                                      . '<span class="hidden_Mobile_header">'
+                                      .   $menu_link_description
+                                      . '</span>' . PHP_EOL
+                                  :   ""
+                                  ;
+        $target =  $children->target === '_blank' ? '_blank' : '_self';
 
-            foreach ($value->wpse_children as $key => $children) {
-                $column_choice = get_field('column_choice', $children);
-                $menu_link_description = get_field('menu_link_description', $children);
-                $description = !empty($menu_link_description) ? '<br><span class="hidden_Mobile_header">' . $menu_link_description . '</span>' . "\n" : "";
-                $target =  $children->target === '_blank' ? '_blank' : '_self';
-
-                if ($column_choice === 'one') {
-                    $column_one[] = '<li><a role="menuitem" target="'.$target.'" href="' . $children->url . '">' . $children->title . '<svg xmlns="http://www.w3.org/2000/svg"
-                        width="16" height="16" fill="#990000" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd"
-                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
-                        </svg>' . $description . '</a></li>' . "\n";
-                }
-
-                if ($column_choice === 'two') {
-                    $column_two[] = '<li><a role="menuitem" target="'.$target.'" href="' . $children->url . '">' . $children->title . '<svg xmlns="http://www.w3.org/2000/svg"
-                        width="16" height="16" fill="#990000" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd"
-                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
-                        </svg>' . $description . '</a></li>' . "\n";
-                }
-            }
-
-            $menu_list .= '<div class="col-lg-4 col-12">' . "\n";
-            $menu_list .= '<div class="col-megamenu">' . "\n";
-            $menu_list .= '<ul class="list-unstyled">' . "\n";
-            $menu_list .= implode("\n", $column_one);
-            $menu_list .= '</ul>' . "\n";
-            $menu_list .= '</div>' . "\n";
-            $menu_list .= '</div>' . "\n";
-
-            if (count($column_two) > 0) {
-                $menu_list .= '<div class="col-lg-4 col-12">' . "\n";
-                $menu_list .= '<div class="col-megamenu">' . "\n";
-                $menu_list .= '<ul class="list-unstyled">' . "\n";
-                $menu_list .= implode("\n", $column_two);
-                $menu_list .= '</ul>' . "\n";
-                $menu_list .= '</div>' . "\n";
-                $menu_list .= '</div>' . "\n";
-            }
-
-            $menu_list .= '</div>' . "\n";
-            $menu_list .= '</div>' . "\n";
-            $menu_list .= '</div>' . "\n";
-            $menu_list .= '</li>' . "\n";
+        if($column_choice === 'one') {
+          $column_one[] =
+              '<li>'
+            .   '<a role="menuitem" '
+            .       'target="'.$target.'" '
+            .       'href="' . $children->url . '"'
+            .   '>'
+            .     $children->title
+            .     '<svg xmlns="http://www.w3.org/2000/svg" '
+            .           'width="16" '
+            .           'height="16" '
+            .           'fill="#990000" '
+            .           'class="bi '
+            .           'bi-chevron-right" '
+            .           'viewBox="0 0 16 16"> '
+            .       '<path fill-rule="evenodd" '
+            .         'd="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 '
+            .         '.708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 '
+            .         '2.354a.5.5 0 0 1 0-.708" /> '
+            .     '</svg>'
+            .     $description
+            .   '</a>'
+            . '</li>' . PHP_EOL;
         }
+
+        if($column_choice === 'two') {
+          $column_two[] =
+              '<li>'
+            .   '<a role="menuitem" '
+            .       'target="'.$target.'" '
+            .       'href="' . $children->url . '"'
+            .   '>'
+            .     $children->title
+            .     '<svg xmlns="http://www.w3.org/2000/svg" '
+            .           'width="16" '
+            .           'height="16" '
+            .           'fill="#990000" '
+            .           'class="bi bi-chevron-right" '
+            .           'viewBox="0 0 16 16"'
+            .     '>'
+            .       '<path  fill-rule="evenodd" '
+            .               'd="M4.646 1.646a.5.5 0 0 1 .708 '
+            .               '0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 '
+            .               '1-.708-.708L10.293 8 4.646 2.354a.5.5 '
+            .               '0 0 1 0-.708" '
+            .       '/>'
+            .     '</svg>'
+            .     $description
+            .   '</a>'
+            . '</li>'
+            . PHP_EOL
+            ;
+        }
+      }
+
+      $menu_list .=   '<div class="col-lg-4 col-12">' . PHP_EOL
+                    .   '<div class="col-megamenu">' . PHP_EOL
+                    .     '<ul class="list-unstyled">' . PHP_EOL
+                    .       implode(PHP_EOL, $column_one)
+                    .     '</ul>' .  PHP_EOL
+                    .   '</div>' . PHP_EOL
+                    . '</div>' . PHP_EOL
+                    ;
+
+      if(count($column_two) > 0) {
+        $menu_list .=   '<div class="col-lg-4 col-12">' . PHP_EOL
+                      .   '<div class="col-megamenu">' . PHP_EOL
+                      .     '<ul class="list-unstyled">' . PHP_EOL
+                      .       implode(PHP_EOL, $column_two)
+                      .     '</ul>' . PHP_EOL
+                      .   '</div>' . PHP_EOL
+                      . '</div>' . PHP_EOL
+                      ;
+      }
+
+      $menu_list .=           '</div>' . PHP_EOL
+                      .     '</div>' . PHP_EOL
+                      .   '</div>' . PHP_EOL
+                      . '</li>' . PHP_EOL
+                      ;
+      }
     }
 
-    $menu_list .= '</ul>' . "\n";
-
+    $menu_list .= '</ul>' . PHP_EOL;
     echo $menu_list;
 }
 
@@ -247,7 +343,7 @@ function create_section_navigation()
     ));
 
     // If no child pages, get siblings and current page
-    if (empty($pages_to_display)) {
+    if(empty($pages_to_display)) {
         $siblings_and_current = get_pages(array(
             'child_of' => wp_get_post_parent_id($current_page_id),
             'parent' => wp_get_post_parent_id($current_page_id),
@@ -259,7 +355,7 @@ function create_section_navigation()
         $pages_to_display = $siblings_and_current;
 
         // If no sibling or children pages exist, parent page + its siblings display
-        if (count($pages_to_display) == 1) {
+        if(count($pages_to_display) == 1) {
             $siblings_and_current =   get_pages(array(
                 'child_of' => wp_get_post_parent_id($pages_to_display[0]->post_parent),
                 'parent' => wp_get_post_parent_id($pages_to_display[0]->post_parent),
@@ -283,11 +379,11 @@ function create_section_navigation()
 function buildTree(array &$elements, $parentId = 0)
 {
     $branch = array();
-    foreach ($elements as &$element) {
+    foreach($elements as &$element) {
 
-        if ($element->menu_item_parent == $parentId) {
+        if($element->menu_item_parent == $parentId) {
             $children = buildTree($elements, $element->ID);
-            if ($children)
+            if($children)
                 $element->wpse_children = $children;
             else
                 $element->wpse_children = array();
@@ -356,12 +452,12 @@ function custom_breadcrumb()
     $count = count($breadcrumbs);
 
     // print_r($breadcrumbs);
-    foreach ($breadcrumbs as $key => $crumb) {
-        if ($crumb['url']) {
-            if ($key == 0) {
+    foreach($breadcrumbs as $key => $crumb) {
+        if($crumb['url']) {
+            if($key == 0) {
                 echo '<li class="breadcrumb-item"><a href="' . esc_url(home_url('/')) . '">' . get_the_title(get_option('page_on_front')) . '</a></li>';
             }
-            elseif ($key == $count - 2 ) {
+            elseif($key == $count - 2 ) {
                 if($crumb['post_id'] == get_option('page_on_front')){
                     echo '';
                 }else{
@@ -405,7 +501,7 @@ add_action('enqueue_block_editor_assets', 'custom_enqueue_block_editor_assets');
 
 function wpdocs_excerpt_more($more)
 {
-    if (!is_single()) {
+    if(!is_single()) {
         $more = sprintf(
             '<br><a class="Button_Link" href="%1$s">%2$s</a>',
             get_permalink(get_the_ID()),
@@ -421,7 +517,7 @@ add_filter('excerpt_more', 'wpdocs_excerpt_more');
 
 //     $text = strip_shortcodes(wp_strip_all_tags($content));
 //     $words = explode(' ', $text);
-//     if (count($words) > $length) {
+//     if(count($words) > $length) {
 //         $excerpt = array_slice($words, 0, $length);
 //         $excerpt = implode(' ', $excerpt) . sprintf(
 //             '<br><a class="Button_Link" href="%1$s">%2$s</a>',
@@ -434,7 +530,7 @@ add_filter('excerpt_more', 'wpdocs_excerpt_more');
 //     return $excerpt;
 // }
 // function custom_get_the_excerpt($post) {
-//     if (has_excerpt($post->ID)) {
+//     if(has_excerpt($post->ID)) {
 //         return get_the_excerpt($post->ID);
 //     } else {
 //         $content = get_the_content($post->ID);
@@ -451,13 +547,12 @@ add_filter('excerpt_more', 'wpdocs_excerpt_more');
 
 // Add this to your theme's functions.php file
 
-function custom_block_excerpt($excerpt)
-{
-    if (has_block('custom/container', get_the_content())) {
+function custom_block_excerpt($excerpt) {
+    if(has_block('custom/container', get_the_content())) {
         // Extract content from your custom container block
         $content = get_the_content();
         // Use a regular expression to match your custom block
-        if (preg_match('/<!-- wp:custom\/container -->(.*?)<!-- \/wp:custom\/container -->/s', $content, $matches)) {
+        if(preg_match('/<!-- wp:custom\/container -->(.*?)<!-- \/wp:custom\/container -->/s', $content, $matches)) {
             $content_without_h1 = preg_replace('/<h1[^>]*>.*?<\/h1>/is', '', $matches[1]);
 
             // Generate excerpt without <h1> content
